6b719ed67a4a75c2ae274072cc2bdea0c17063dd
Jon Ruttan <jonruttan@gmail.com>
2024-07-12 12:00:00 -0400

    feat(1/chapter-1.1): add to project

---
+++ src/1-chapter-1/chapter-1.1.rkt
@@ -0,0 +1,61 @@
+#lang racket
+
+(require "prelude/turtles.rkt")
+
+;; Figure 1.1a, p.4#fig.1.1a
+;; 
+;; ; Turtle starts
+;;
+
+; ACTION: Save snapshot fig.1.1a
+
+
+;; Figure 1.1b, p.4#fig.1.1b
+;;
+;; FORWARD 100
+;;
+(forward 100)
+
+; ACTION: Save snapshot fig.1.1b
+
+
+;; Figure 1.1c, p.4#fig.1.1c
+;;
+;; RIGHT 90
+;;
+(right 90)
+
+; ACTION: Save snapshot fig.1.1c
+
+
+;; Figure 1.1d, p.4#fig.1.1d
+;;
+;; FORWARD 150
+;; LEFT 45
+;;
+(forward 150)
+(left 45)
+
+; ACTION: Save snapshot fig.1.1d
+
+
+;; Figure 1.1e, p.4#fig.1.1e
+;;
+;; BACK 100
+;;
+(back 100)
+
+; ACTION: Save snapshot fig.1.1e
+
+
+;; Figure 1.1f, p.4#fig.1.1f
+;;
+;; LEFT 45
+;; PENUP
+;; FORWARD 100
+;;
+(left 45)
+(penup)
+(forward 100)
+
+; ACTION: Save snapshot fig.1.1f
